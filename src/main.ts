c69deddd74b8f268209ad5fba276564e65f8c99f
Tarik791 <terzotarik@gmail.com>
2025-01-02 08:58:27 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`)
  
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors()
   await app.listen(port)
 }
 bootstrap()
