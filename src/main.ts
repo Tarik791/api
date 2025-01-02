acaf567e6812cd230c7e3ed30a6c6ee52dfe80e6
Tarik791 <terzotarik@gmail.com>
2025-01-02 09:50:24 +0100

    cors 4

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-      origin: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
-    });
+    origin: 'https://www.infoontheweb.com',  
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });  
   await app.listen(process.env.PORT ?? 3000);  
 }
 
