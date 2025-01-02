6fcd8ce6c20ebc3f4d0a5c6a2d0920e88a659ad2
Tarik791 <terzotarik@gmail.com>
2025-01-02 09:07:56 +0100

    cors 2

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
- 
-const port = process.env.PORT || 3000
-console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`)
- 
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.enableCors()
-  await app.listen(port)
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://infoontheweb.com'], // Dodane obe dozvoljene adrese
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Include cookies, if needed
+  });
+
+  const port = process.env.PORT || 3000;
+  console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`);
+  await app.listen(port);
 }
-bootstrap()
+
+bootstrap();
