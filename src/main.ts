d0200c478d58b2397d673ba76ec444863a8a1ee2
Tarik791 <terzotarik@gmail.com>
2025-01-02 09:37:49 +0100

    cors 4

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://infoontheweb.com'], // Dodane obe dozvoljene adrese
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, // Include cookies, if needed
-  });
-
-  const port = process.env.PORT || 3000;
-  console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`);
-  await app.listen(port);
+      origin: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    });
+  await app.listen(process.env.PORT ?? 3000);  
 }
 
 bootstrap();
